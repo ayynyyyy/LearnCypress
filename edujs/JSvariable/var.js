2615b3c97b0724b81510b6714a1374b2f460c8d8
ayynyyyy <ayynyyyy@users.noreply.github.com>
2025-02-01 01:37:40 +0700

    tugas

---
+++ edujs/JSvariable/var.js
@@ -0,0 +1,9 @@
+//const name = "aini";
+//name = "eduwork";
+//console.log (name);
+ const student = {
+    nama : "lala",
+    class : 2
+ }
+  student.faculty = "sainteks";
+  console.log(student);
